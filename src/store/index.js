2987d7c74817a6fbdb46973d23a93ed2763f3116
demilee06 <chloealmazien6@gmail.com>
2024-09-13 15:31:17 +0200

    added firebase

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,8 @@ import{useCookies} from 'vue-cookies'
 axios.defaults.headers = $cookies.get('token') 
 axios.defaults.withCredentials = true
 
+const apiURL = 'https://capstone-project-yqit.onrender.com'
+
 export default createStore({
   state: {
     users:null,
@@ -24,65 +26,65 @@ export default createStore({
   },
   actions: {
     async getUsers({commit}){
-      let data = await axios.get (`http://localhost:2023/users`)
+      let data = await axios.get (`${apiURL}/users`)
       console.log(data);
-      commit('setUsers',data)
+      commit('setUsers',data)   
     },
-    async getUser({commit}){
-      let data = await axios.get(`http://localhost:2023/users/:id`)
+    async getUser({commit},id){
+      let data = await axios.get(`${apiURL}/users/${prod.id}`)
       console.log(data);
       
       commit('setUsers',data)
     },
     async insertUser({commit},info){
-      let data = await axios.post(`http://localhost:2023/users/insert`)
+      let data = await axios.post(`${apiURL}/users/insert`)
       console.log(data);
       
       commit('setUsers',data)
     },
     async loginUser ({commit}){
-      let {data} = await axios.post(`http://localhost:2023/users/login`)
+      let {data} = await axios.post(`${apiURL}/users/login`)
       console.log(data);
       /*eslint-disable*/
       $cookies.set('token',data.token)
     },
-    async updateUser({commit}){
-      let data = await axios.patch(`http://localhost:2023/users/update/:id`)
+    async updateUser({commit},id){
+      let data = await axios.patch(`${apiURL}/users/update/${prod.id}`)
       console.log(data);
       
       commit('setUsers',data)
     },
-    async deleteUser({commit}){
-      let data = await axios.delete(`http://localhost:2023/users/update/:id`)
+    async deleteUser({commit},id){
+      let data = await axios.delete(`${apiURL}/users/update/${prod.id}`)
       console.log(data);
       
       commit('setUsers',data)
     },
     async getProducts({commit}){
-      let {data} = await axios.get(`http://localhost:2023/products`)
+      let {data} = await axios.get(`${apiURL}/products`)
       console.log(data);
       commit('setProducts',data)
     },
-    async getProduct({commit}){
-      let data = await axios.get(`http://localhost:2023/products/:id`)
+    async getProduct({commit},id){
+      let data = await axios.get(`${apiURL}/products/${prod.id}`)
       console.log(data);
       
       commit('setProducts',data)
     },
     async addProduct ({commit}){
-      let data = await axios.post(`http://localhost:2023/products/addProduct`)
+      let data = await axios.post(`${apiURL}/products/addProduct`)
       console.log(data);
       
       commit('setProducts',data)
     },
-    async updateProduct ({commit}){
-      let data = await axios.patch(`http://localhost:2023/products/update/:id`)
+    async updateProduct ({commit},id){
+      let data = await axios.patch(`${apiURL}/products/update/${prod.id}`)
       console.log(data);
       
       commit('setProducts',data)
     },
-    async deleteProduct ({commit}){
-      let data = await axios.delete(`http://localhost:2023/products/delete/:id`)
+    async deleteProduct ({commit},id){
+      let data = await axios.delete(`${apiURL}/products/delete/${prod.id}`)
       console.log(data);
       
       commit('setProducts',data)
